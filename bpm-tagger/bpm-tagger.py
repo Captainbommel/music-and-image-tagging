c4552460257022bcfa0740c0fe4229e281a018e7
Lars Dieper <ldieper1@gmail.com>
2024-07-22 17:56:32 +0200

    linted the code

--- bpm-tagger/bpm-tagger.py
+++ bpm-tagger/bpm-tagger.py
@@ -1,111 +1,115 @@
 import json
 import os
+from time import sleep
 import librosa as rosa
 import requests
-import os
 from dotenv import load_dotenv
 
 # Load the environment variables
 load_dotenv()
-PROJECT_PATH = os.getenv('PROJECT_PATH')
+PROJECT_PATH = os.getenv("PROJECT_PATH")
+
 
 def get_eagle_and_deezer_ids(limit=1000000):
-    '''Gets the ids and urls of all songs in the database.'''
+    """Gets the ids and urls of all songs in the database."""
     url = f"http://localhost:41595/api/item/list?limit={limit}"
     response = requests.get(url)
     data = response.json()
 
     if response.status_code != 200:
-        print('Error:', response.status_code)
+        print("Error:", response.status_code)
         return [], []
-    
-    l = len(data["data"])
-    print(f"Tracks found: {l}")
-    eagle_ids = [None] * l
-    deezer_ids = [None] * l
-    for i in range(l):
+
+    length = len(data["data"])
+    print(f"Tracks found: {length}")
+    eagle_ids = [None] * length
+    deezer_ids = [None] * length
+    for i in range(length):
         eagle_ids[i] = data["data"][i]["id"]
         deezer_ids[i] = data["data"][i]["annotation"]
 
     return eagle_ids, deezer_ids
 
+
 def get_preview_url(id):
-    '''Gets the 30 second preview url of a song.'''
+    """Gets the 30 second preview url of a song."""
     url = f"https://api.deezer.com/track/{id}"
     response = requests.get(url)
     data = response.json()
 
     if response.status_code != 200:
-        print('Error:', response.status_code)
+        print("Error:", response.status_code)
         return ""
-    
+
     return data["preview"]
 
 
 def download_preview(deezer_id, path):
-    '''Downloads the 30 second preview of a song.'''
+    """Downloads the 30 second preview of a song."""
     url = get_preview_url(deezer_id)
     if url == "":
         print("No preview available.")
         return False
     response = requests.get(url)
     if response.status_code != 200:
-        print('Error:', response.status_code)
+        print("Error:", response.status_code)
         return False
-    with open(path, 'wb') as f:
+    with open(path, "wb") as f:
         f.write(response.content)
         f.close()
     return True
 
 
 def get_bpm(path):
-    '''Estimates the bpm of a given mp3 file.'''
+    """Estimates the bpm of a given mp3 file."""
     y, sr = rosa.load(path)
     tempo, beat_frames = rosa.beat.beat_track(y=y, sr=sr)
     return tempo[0]
+
 
 def round_to_multiple(number, multiple):
     return multiple * round(number / multiple)
 
 
 def add_bpm_tag(eagle_id, bpm):
-    '''Adds the bpm tag to the track with the given id in the database.'''
+    """Adds the bpm tag to the track with the given id in the database."""
 
     # get the tags of the track
     url = f"http://localhost:41595/api/item/info?id={eagle_id}"
-    
+
     response = requests.get(url)
     data = response.json()
     tags = data["data"]["tags"]
 
     if response.status_code != 200:
-        print('Error:', response.status_code)
+        print("Error:", response.status_code)
         return
 
     # update the tags
     data = {
         "id": eagle_id,
-        "tags": tags + [str(bpm)+"BPM"] if str(bpm)+"BPM" not in tags else tags,
+        "tags": tags + [str(bpm) + "BPM"] if str(bpm) + "BPM" not in tags else tags,
     }
     url = "http://localhost:41595/api/item/update"
-    headers = {'Content-Type': 'application/json'}
+    headers = {"Content-Type": "application/json"}
     response = requests.post(url, data=json.dumps(data), headers=headers)
 
     if response.status_code != 200:
-        print('Error:', response.status_code)
+        print("Error:", response.status_code)
+
 
 def add_no_30sec_preview_tag(eagle_id):
-    '''Adds the "no 30 second preview" tag to the track with the given id in the database.'''
+    """Adds the "no 30 second preview" tag to the track with the given id in the database."""
 
     # get the tags of the track
     url = f"http://localhost:41595/api/item/info?id={eagle_id}"
-    
+
     response = requests.get(url)
     data = response.json()
     tags = data["data"]["tags"]
 
     if response.status_code != 200:
-        print('Error:', response.status_code)
+        print("Error:", response.status_code)
         return
 
     # update the tags
@@ -114,21 +118,20 @@ def add_no_30sec_preview_tag(eagle_id):
         "tags": tags + ["Geolocked"] if "Geolocked" not in tags else tags,
     }
     url = "http://localhost:41595/api/item/update"
-    headers = {'Content-Type': 'application/json'}
+    headers = {"Content-Type": "application/json"}
     response = requests.post(url, data=json.dumps(data), headers=headers)
 
     if response.status_code != 200:
-        print('Error:', response.status_code)
+        print("Error:", response.status_code)
 
 
 def delete_preview(path):
-    '''Deletes the downloaded preview file.'''
+    """Deletes the downloaded preview file."""
     if os.path.exists(path):
         os.remove(path)
         print("Preview file deleted.")
     else:
         print("Preview file does not exist.")
-
 
 
 ########
@@ -137,9 +140,11 @@ def delete_preview(path):
 
 eagle_ids, deezer_ids = get_eagle_and_deezer_ids()
 
-for i in range(len(eagle_ids)):
+start = 500
+end = 6000
+for i in range(start, len(eagle_ids)):
     deezer_id = deezer_ids[i]
-    if int(deezer_id) < 0: 
+    if int(deezer_id) < 0:
         continue
     eagle_id = eagle_ids[i]
     path = f"{PROJECT_PATH}/bpm-tagger/deezer-preview-mp3/{eagle_id}_{deezer_id}.mp3"
@@ -151,5 +156,8 @@ for i in range(len(eagle_ids)):
     else:
         add_no_30sec_preview_tag(eagle_id)
 
-    if i == 10:
+    print(f"Track {i+1} of {len(eagle_ids)}")
+    sleep(5)
+
+    if i == end:
         break
--- correct-date-finder/correct-date-finder.py
+++ correct-date-finder/correct-date-finder.py
@@ -7,11 +7,13 @@ from PIL import Image
 from dotenv import load_dotenv
 
 
+
 # Load the environment variables
 load_dotenv()
-PROJECT_PATH = os.getenv('PROJECT_PATH')
-VAULT_PATH = os.getenv('VAULT_PATH')
-IMAGE_LIBRARY_NAME = os.getenv('IMAGE_LIBRARY_NAME')
+PROJECT_PATH = os.getenv("PROJECT_PATH")
+VAULT_PATH = os.getenv("VAULT_PATH")
+IMAGE_LIBRARY_NAME = os.getenv("IMAGE_LIBRARY_NAME")
+
 
 def get_eagle_name(eagle_id):
     url = f"http://localhost:41595/api/item/info?id={eagle_id}"
@@ -20,10 +22,11 @@ def get_eagle_name(eagle_id):
     name = data["data"]["name"]
 
     if response.status_code != 200:
-        print('Error:', response.status_code)
+        print("Error:", response.status_code)
         return None
 
     return name
+
 
 def get_eagle_id(name):
     url = f"http://localhost:41595/api/item/list?limit=1&name={name}"
@@ -31,7 +34,7 @@ def get_eagle_id(name):
     data = response.json()
 
     if response.status_code != 200:
-        print('Error:', response.status_code)
+        print("Error:", response.status_code)
         return None
 
     if data["data"][0]["name"] == name:
@@ -39,67 +42,71 @@ def get_eagle_id(name):
 
     return None
 
+
 def get_eagle_ids_and_names(limit=1000000):
-    '''Returns the eagle ids and the names of the files'''
+    """Returns the eagle ids and the names of the files"""
     url = f"http://localhost:41595/api/item/list?limit={limit}"
     response = requests.get(url)
     data = response.json()
 
     if response.status_code != 200:
-        print('Error:', response.status_code)
+        print("Error:", response.status_code)
         return []
-    
-    l = len(data["data"])
-    print(f"Number of Pictures found: {l}")
-    ids = [None] * l
-    names = [None] * l
-    for i in range(l):
+
+    length = len(data["data"])
+    print(f"Number of Pictures found: {length}")
+    ids = [None] * length
+    names = [None] * length
+    for i in range(length):
         ids[i] = data["data"][i]["id"]
         names[i] = data["data"][i]["name"]
 
     return ids, names
 
+
 def get_date(eagle_id):
     url = f"http://localhost:41595/api/item/info?id={eagle_id}"
-    
+
     response = requests.get(url)
     data = response.json()
     timestamp = data["data"]["btime"]
 
     if response.status_code != 200:
-        print('Error:', response.status_code)
+        print("Error:", response.status_code)
         return
-    
+
     return datetime.datetime.fromtimestamp(timestamp / 1000)
+
 
 def update_btime(eagle_id, new_time):
     path = f"{VAULT_PATH}/{IMAGE_LIBRARY_NAME}/images/{eagle_id}.info/metadata.json"
     timestamp = int(new_time.timestamp() * 1000)
 
-    with open(path, 'r') as file:
+    with open(path, "r") as file:
         metadata = json.load(file)
-    file.close() #? needed?
+    file.close()
 
-    metadata['btime'] = timestamp
-    with open(path, 'w') as file:
+    metadata["btime"] = timestamp
+    with open(path, "w") as file:
         json.dump(metadata, file)
-    file.close() #? needed?
+    file.close()
 
 
 def get_date_from_string(str):
-    pattern = [r'VID-(\d{8})-WA\d{4}', r'IMG-(\d{8})-WA\d{4}', r'PXL-(\d{8})-\d{9}']
+    pattern = [r"VID-(\d{8})-WA\d{4}", r"IMG-(\d{8})-WA\d{4}", r"PXL-(\d{8})-\d{9}"]
 
     for regex_pattern in pattern:
         match = re.search(regex_pattern, str)
         if match:
             #? import datetime 
-            return datetime.datetime.strptime(match.group(1), '%Y%m%d')
+            return datetime.strptime(match.group(1), "%Y%m%d")
     return None
+
 
 def get_date_taken_from_exif(eagle_id):
     path = f"{VAULT_PATH}/{IMAGE_LIBRARY_NAME}/images/{eagle_id}.info/"
 
-    with open(path + "metadata.json", 'r') as file:
+    with open(path + "metadata.json", "r") as file:
         metadata = json.load(file)
     file.close()
 
@@ -111,30 +118,29 @@ def get_date_taken_from_exif(eagle_id):
     return datetime.datetime.strptime(exif[36867], "%Y:%m:%d %H:%M:%S")
 
 
-
 ########
 # Main #
 ########
 
 # get all eagle ids and names
 ids, names = get_eagle_ids_and_names()
-#print(names[0], ids[0])
 
 # find new patterns
-pattern = [r'VID-(\d{8})-WA\d{4}',                                  # whatsapp video
-           r'IMG-(\d{8})-WA\d{4}',                                  # whatsapp image
-           r'PXL-(\d{8})-\d{9}',                                    # pixel image
-           r'PXL_(\d{8})_\d{9}',                                    # pixel image
-           r'(\d{8})_\d{9}_iOS',                                    # iOS image
-           r'VID-(\d{8})-\d{6}',                                    # unknown source video
-           r'VID_(\d{8})_\d{6}',                                    # unknown source video
-           r'Screenshot_(\d{8})-\d{6}',                             # screenshot
-           r'Screenshot_(\d{4})-(\d{2})-(\d{2})-\d{2}-\d{2}-\d{2}', # screenshot
-           r'\d{3}-DSC\d{5}-(\d{8})-\d{4}-\d{2}-\d{2}-',            # unknown source image
-           r'IMG_(\d{8})_\d{6}',                                    # unknown source image
-           r'(\d{8})\d{8}-',                                        # switch screenshot
-           r'(\d{13})-.{8}(-)',
-           ]  
+pattern = [
+    r"VID-(\d{8})-WA\d{4}",  # whatsapp video
+    r"IMG-(\d{8})-WA\d{4}",  # whatsapp image
+    r"PXL-(\d{8})-\d{9}",  # pixel image
+    r"PXL_(\d{8})_\d{9}",  # pixel image
+    r"(\d{8})_\d{9}_iOS",  # iOS image
+    r"VID-(\d{8})-\d{6}",  # unknown source video
+    r"VID_(\d{8})_\d{6}",  # unknown source video
+    r"Screenshot_(\d{8})-\d{6}",  # screenshot
+    r"Screenshot_(\d{4})-(\d{2})-(\d{2})-\d{2}-\d{2}-\d{2}",  # screenshot
+    r"\d{3}-DSC\d{5}-(\d{8})-\d{4}-\d{2}-\d{2}-",  # unknown source image
+    r"IMG_(\d{8})_\d{6}",  # unknown source image
+    r"(\d{8})\d{8}-",  # switch screenshot
+    r"(\d{13})-.{8}(-)",
+]
 
 skip = []
 
@@ -150,7 +156,9 @@ for i in range(start, len(names)):
         match = re.search(regex_pattern, names[i])
         if match:
             if re.compile(regex_pattern).groups == 3:
-                date = datetime.datetime.strptime(match.group(1) + match.group(2) + match.group(3), '%Y%m%d')
+                date = datetime.datetime.strptime(
+                    match.group(1) + match.group(2) + match.group(3), "%Y%m%d"
+                )
                 update_btime(ids[i], date)
 
             elif re.compile(regex_pattern).groups == 2:
@@ -158,18 +166,18 @@ for i in range(start, len(names)):
                 update_btime(ids[i], date)
 
             else:
-                date = datetime.datetime.strptime(match.group(1), '%Y%m%d')
+                date = datetime.datetime.strptime(match.group(1), "%Y%m%d")
                 update_btime(ids[i], date)
 
             found = True
-            break 
+            break
 
-    if found == False:
-        date = get_date_taken_from_exif(ids[i]) 
-        if date != None:
+    if found is False:
+        date = get_date_taken_from_exif(ids[i])
+        if date is not None:
             update_btime(ids[i], date)
         else:
             missing.append(names[i])
-            print(f"the date for: '{names[i]}' could not be found") 
+            print(f"the date for: '{names[i]}' could not be found")
 
 print(missing)
